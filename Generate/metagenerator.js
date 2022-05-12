a32f9f144a21270496485e4865b9efb290cf1276
CaptainUnknown <captainunknown7@gmail.com>
2022-05-12 23:20:01 +0500

    Changed image URL & name

--- Generate/metagenerator.js
+++ Generate/metagenerator.js
@@ -1,9 +1,9 @@
 for (let i = 1; i <= 500; i++) {
     let j = getNumber(i);
     obj = {
-        "name": "JPGO Club VIP Pass #" + j,
+        "name": "JPGO Club VIP Pass",
         "description": "在这里，我们交流与分享：日本居住权最新申请方法，在日财税筹划，商业合作或投资机会，日本企业招聘信息，各大名校招生信息，生活，旅游，居住常用日语等等一切与在日生活相关的前沿资讯。",
-        "image": "https://gateway.pinata.cloud/ipfs/QmdrHGdpcWeF8ZbXXnz8iLkBtwRqHXREZowDoXhE2bwZUz"
+        "image": "ipfs://QmdrHGdpcWeF8ZbXXnz8iLkBtwRqHXREZowDoXhE2bwZUz"
     };
     var json = JSON.stringify(obj);
     var fs = require('fs');
